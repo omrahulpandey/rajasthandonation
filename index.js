ae2dbaddcac696369a529a2f2437636ae1d20c29
Rahul <60060371+omrahulpandey@users.noreply.github.com>
2022-10-13 10:56:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const generatePDF = async (name) => {
 
   // Draw a string of text diagonally across the first page
   firstPage.drawText(name, {
-    x: 340,
+    x: 330,
     y: 368,
     size: 36,
     font: SanChezFont,
